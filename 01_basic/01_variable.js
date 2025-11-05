20a7b4e2a76e3ed61f8fac0bdddbb66b599495eb
Akash Daud <142325556+Akash-Daud@users.noreply.github.com>
2025-11-05 07:19:36 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+// We have the 3 type of the variable 
+//Const , let , var
+
+const accountId = 12345
+let accountEmail ="akash@gmail.com";
+var accountPassword ="98765";
+acccountCity ="Mumbai"  // by difult var
+
+//accountId = 12345678  // not allowed
+
+accountEmail = "daud@gmail.com";
+accountPassword ="123"
+acccountCity ="Pune";
+
+
+/* Prefer not to use var
+becouse of issue in block scope and functionalscope */
+
+console.table([ accountId ,accountEmail,accountId,accountPassword])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"   // trate all js code as newer version for the es6
+
+ //alert(3+3)  // we are using the node.js , not browser
+
+ let name = "Akash"
+ let age = 18 
+ let isLoggin = false
+
+
+ // number => 2 to the power of 53
+ // bigint
+ //string => ""
+ //boolean => true/false
+ //null        let state= null
+// undefined   ex let state
+// symbol       
